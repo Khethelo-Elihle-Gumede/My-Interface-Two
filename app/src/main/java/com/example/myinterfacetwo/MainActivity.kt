c691dff027ae1a43787a593c098074b488744bb6
Khethelo-Elihle-Gumede <ST10483277@myemeris.edu.za>
2026-03-12 09:18:40 +0200

    My First Commit

---
+++ app/src/main/java/com/example/myinterfacetwo/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myinterfacetwo
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myinterfacetwo.ui.theme.MyInterfaceTwoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView (R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btnShowImage)
+        val image = findViewById<ImageView>(R.id.imgProfile)
+
+        button.setOnClickListener {
+            image.visibility = ImageView.VISIBLE
+
+
+        }
+
+
+
+    }
+}
